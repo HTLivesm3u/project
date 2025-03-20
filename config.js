bf4a7acb15660e8395980a51cb9225ab41fe9f9a
unkown_4777 <169054920+HTLivesm3u@users.noreply.github.com>
2025-03-20 17:44:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
+// config.js (Stores API Keys)
 const CONFIG = {
     SPOTIFY_CLIENT_ID: "7afe6b39346f4bcc8654c141fe2a6136",
     SPOTIFY_CLIENT_SECRET: "119278484c2f4c28ac893ea3324bfe84",
     YOUTUBE_API_KEY: "AIzaSyAwKYDbklOiCO1o3Dr7uM-xISbjksPkgDk"
 };
+export default CONFIG;
