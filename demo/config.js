02cc9a202ee25f2c68f5cb3e70c1b07d4f31b8fb
unkown_4777 <169054920+HTLivesm3u@users.noreply.github.com>
2025-03-21 11:55:09 +0530

    Update config.js

--- demo/config.js
+++ demo/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
-    SPOTIFY_CLIENT_ID: "your_spotify_client_id",
-    SPOTIFY_CLIENT_SECRET: "your_spotify_client_secret",
-    JIOSAAVN_API_URL: "https://jiosaavn-api-url.com"
+    SPOTIFY_CLIENT_ID: "7afe6b39346f4bcc8654c141fe2a6136",
+    SPOTIFY_CLIENT_SECRET: "119278484c2f4c28ac893ea3324bfe84",
+    JIOSAAVN_API_BASE: "https://saavn.dev/api"
 };
 
 export default CONFIG;
