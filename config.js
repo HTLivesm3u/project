30d6a7e5c214a101d43e9ebda2118cebf7d7adda
unkown_4777 <169054920+HTLivesm3u@users.noreply.github.com>
2025-03-21 11:43:21 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,7 @@
 const CONFIG = {
     SPOTIFY_CLIENT_ID: "7afe6b39346f4bcc8654c141fe2a6136",
     SPOTIFY_CLIENT_SECRET: "119278484c2f4c28ac893ea3324bfe84",
-    YOUTUBE_API_KEY: "AIzaSyAwKYDbklOiCO1o3Dr7uM-xISbjksPkgDk"
+    JIOSAAVN_API_BASE: "https://saavn.dev/api"
+    // YOUTUBE_API_KEY: "AIzaSyAwKYDbklOiCO1o3Dr7uM-xISbjksPkgDk",
 };
 export default CONFIG;
